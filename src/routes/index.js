1204dddfe3acd30aa165d9ddb3735727db745686
andres101010 <andreschapu5295@gmail.com>
2023-08-01 12:30:51 -0400

    index modified

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,17 @@
 
 const express = require('express');
 const router = express.Router();
-
+// Bienvenida
 router.get('/',(req,res)=>{
-    res.send("Hello World")
+    res.send("Bienvenido a Unsplash")
 })
+
+// llamar todas las imagenes
+
+// llamar imagen por ID
+
+// Crear imagen
+
+// Borrar imagen
 
 module.exports = router;
